7ebb4ffe2e081ec0b2238489303095b64289acaf
Florian Binder <florian.binder0@gmail.com>
2020-04-05 14:56:20 +0200

    premier commit du front-end de PlaceToVisit

---
+++ src/main/java/com/projet/placetovisit/PlaceToVisitItem.java
@@ -0,0 +1,5 @@
+package com.projet.placetovisit;
+
+public class PlaceToVisitItem {
+	
+}
---
+++ src/test/java/com/projet/testplacetovisit/PlaceToVisitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet.testplacetovisit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaceToVisitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
