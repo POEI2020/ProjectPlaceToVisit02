472c31f4263b54ba80b75ecfd7844cc4fed8b5e7
Julien <julien.kiener@yahoo.com>
2020-04-07 10:09:30 +0200

    adaptation war packaging

--- src/main/java/com/projet/placestovisit/PlacesToVisitApplication.java
+++ src/main/java/com/projet/placestovisit/PlacesToVisitApplication.java
@@ -2,9 +2,17 @@ package com.projet.placestovisit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PlacesToVisitApplication {
+public class PlacesToVisitApplication extends SpringBootServletInitializer{
+
+	//Override nécessaire pour packager en war
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(PlacesToVisitApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PlacesToVisitApplication.class, args);
