e000a54e60c2ecfaae2216e897799eca5d1de7bc
Florian Binder <florian.binder0@gmail.com>
2020-04-07 16:23:35 +0200

    Problème Thymeleaf page formulaire

--- src/main/java/com/projet/placestovisit/PlacesToVisitController.java
+++ src/main/java/com/projet/placestovisit/PlacesToVisitController.java
@@ -1,8 +1,75 @@
 package com.projet.placestovisit;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class PlacesToVisitController {
+	
+	
+	List<PlacesToVisitItem> placesToVisitItems = new ArrayList<PlacesToVisitItem>();
+	private int index = 0;
+	
+	
+	@GetMapping("/MyPlacesToVisit.html")
+	public ModelAndView getPlacesList() {
+		
+		String viewName = "myPlacesToVisit";
+		Map<String, Object> model = new HashMap<String, Object>();
+		
+		model.put("placesToVisitItems", placesToVisitItems);
+		model.put("numberOfPlaces", placesToVisitItems.size());
+		return new ModelAndView(viewName, model);
+	}
+	
+	
+	
+	@GetMapping("/placesToVisitItemForm.html")
+	public ModelAndView showPlacesToVisitItemForm(@RequestParam(required=false) Integer id) {
+		
+		Map<String,Object> model = new HashMap<String,Object>();
+		
+		PlacesToVisitItem placesToVisitItem = findPlacesToVisitItemById(id);
+		if (placesToVisitItem == null) {
+			model.put("placesToVisitItem", new PlacesToVisitItem());
+		} else {
+			model.put("placesToVisitItem", placesToVisitItem);
+		}
+		return new ModelAndView("placeToVisitItemForm" , model);
+	}
+	
+	private PlacesToVisitItem findPlacesToVisitItemById(Integer id) {
+		for (PlacesToVisitItem placesToVisitItem : placesToVisitItems) {
+			if (placesToVisitItem.getId().equals(id)) {
+				return placesToVisitItem;
+			}
+		}
+		return null;
+	}
+	
+	
+	
+	
+	@PostMapping("/placesToVisitItemForm.html")
+	public ModelAndView submitWatchlistItemForm(PlacesToVisitItem placesToVisitItem) {
+		
+		placesToVisitItem.setId(index++);
+		placesToVisitItems.add(placesToVisitItem);
+		
+		RedirectView redirectView = new RedirectView();
+		redirectView.setUrl("/MyPlacesToVisit");
+		
+		return new ModelAndView(redirectView);
+	}
+		
 	
 }
--- src/main/java/com/projet/placestovisit/PlacesToVisitItem.java
+++ src/main/java/com/projet/placestovisit/PlacesToVisitItem.java
@@ -2,4 +2,67 @@ package com.projet.placestovisit;
 
 public class PlacesToVisitItem {
 	
+	private String listLieu;
+	private String date;
+	private String budget;
+	private String description;
+	private Integer id;
+	public static int index = 0;
+	
+	public PlacesToVisitItem() {
+		this.id = index ++;
+	}
+
+	public PlacesToVisitItem(String listLieu, String date, String budget, String description, Integer id) {
+		super();
+		this.listLieu = listLieu;
+		this.date = date;
+		this.budget = budget;
+		this.description = description;
+		this.id = id;
+	}
+	
+
+	public String getlistLieu() {
+		return listLieu;
+	}
+
+	public void setlistLieu(String listLieu) {
+		this.listLieu = listLieu;
+	}
+	
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+	public String getBudget() {
+		return budget;
+	}
+
+	public void setBudget(String budget) {
+		this.budget = budget;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	
+	
 }
