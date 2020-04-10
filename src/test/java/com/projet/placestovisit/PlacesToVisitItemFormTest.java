89226bf1ea51551f787710bf937dda4c96ac1a8d
Julien <julien.kiener@yahoo.com>
2020-04-10 14:15:45 +0200

    ajout des tests unitaires

--- src/test/java/com/projet/placestovisit/PlacesToVisitItemFormTest.java
+++ src/test/java/com/projet/placestovisit/PlacesToVisitItemFormTest.java
@@ -1,13 +1,48 @@
 package com.projet.placestovisit;
 
 import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class PlacesToVisitItemFormTest {
 
-	@Test
-	public void test() {
+	PlacesToVisitItem PlacesToVisitItemTest;
+	
+	@Before
+	public void init() {
 		
+		PlacesToVisitItemTest=new PlacesToVisitItem("un certain lieu", "une certaine date", "un certain budget", "une certaine description", 2);
+	}
+	
+	@Test
+	public void testLieu() {
+		 PlacesToVisitItemTest.setlistLieu("un lieu génial");
+		 assertEquals( PlacesToVisitItemTest.getlistLieu(), "un lieu génial");
 	}
 
+	@Test
+	public void testDate() {
+		 PlacesToVisitItemTest.setDate("une date géniale");
+		 assertEquals( PlacesToVisitItemTest.getDate(), "une date géniale");
+	}
+	
+	@Test
+	public void testBudget() {
+		 PlacesToVisitItemTest.setBudget("un budget parfait");
+		 assertEquals( PlacesToVisitItemTest.getBudget(), "un budget parfait");
+	}
+	
+	@Test
+	public void testDescription() {
+		 PlacesToVisitItemTest.setDescription("une merveilleuse description");
+		 assertEquals( PlacesToVisitItemTest.getDescription(), "une merveilleuse description");
+	}
+	
+	@Test
+	public void testId() {
+		 PlacesToVisitItemTest.setId(3);
+		 assertEquals( PlacesToVisitItemTest.getId().intValue(), 3);
+	}
 }
