b160567326827662cd8a1c4f9a20d35185d46250
Julien <julien.kiener@yahoo.com>
2020-04-12 13:30:43 +0200

    optimisation du code

--- src/main/java/com/projet/placestovisit/PlacesToVisitController.java
+++ src/main/java/com/projet/placestovisit/PlacesToVisitController.java
@@ -12,10 +12,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
-import java.io.*;
-import java.nio.*;
-import java.sql.*;
-import java.*;
 
 
 @Controller
